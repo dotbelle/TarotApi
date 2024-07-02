81777d1e0d2100243af4248d907d612b9cda0aad
1sabelle <isabelle.schafhauser@gmail.com>
2024-07-02 19:39:51 +0200

    :construction #2 - wip cleanup

--- TarotApi/Suits/SuitApi.cs
+++ TarotApi/Suits/SuitApi.cs
@@ -1,16 +1,15 @@
+using Application.Suits;
+
 namespace TarotApi.Suits;
 
 public static class GetSuits
 {
     public static void MapGetSuits(this WebApplication app)
     {
-        app.MapGet("/api/v{version:apiVersion}/suits",
-                async (GetSuitsQueryHandler handler) => Results.Ok(new ApiResponse<List<GetSuitsListResponse>>(await handler.Get(), Guid.NewGuid().ToString())))
-            .MapToApiVersion(1.0d)
+        app.MapGet("/api/v{version:apiVersion}/suits", (GetSuitsQueryHandler handler) => Results.Ok(handler.GetSuits()))
             .WithOpenApi()
             .WithName("GetSuits")
             .WithSummary("Get suits.")
-            .WithDescription("Get suits")
-            .ProducesCommonErrors();
+            .WithDescription("Get suits");
     }
 }
